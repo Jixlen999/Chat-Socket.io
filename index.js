43d222ad088dcfb8c1d13f73c8bafc2aa2f85a0b
Victoria <viktoriyaivanyutenko@yandex.ru>
2022-05-31 01:15:04 +0300

    Rooms created

--- index.js
+++ index.js
@@ -13,11 +13,17 @@ app.use(express.static(__dirname + '/public'));
 
 
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg, user) => {
-      io.emit('chat message', msg, user);
+    socket.on('send-message', (msg, user) => {
+      socket.broadcast.emit('recieve-message', msg, user);
+    });
+    socket.on('join-room', (room, user, joinMsg) => {
+      socket.join(room);
+      joinMsg(`${user} joined room ${room}`);
     });
 });
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
+//не показывает участиникам комнаты, что кто-то присоединился
--- public/chat.js
+++ public/chat.js
@@ -4,21 +4,30 @@ var form = document.getElementById('form');
 var input = document.getElementById('input');
 let userName = document.getElementById('userName');
 
-const popUp_btn = document.getElementsByClassName('pop-up_body')[0];
+const popUp_enter_btn = document.getElementsByClassName('pop-up_enter-btn')[0];
 const popUp = document.getElementsByClassName('pop-up')[0];
 const messages = document.getElementById('messages');
+const room = document.getElementById('room');
+const popUp_noRoom_btn = document.getElementsByClassName('pop-up_no-room-btn')[0];
 
-
-popUp_btn.addEventListener('submit', closePopUp);
-popUp_btn.addEventListener('keydown', function(e) {
-    if(e.key === 13) {
+popUp_enter_btn.addEventListener('click', () => {
+    if(!room.value) {
+        room.focus();
+        room.style.boxShadow = '0 0 0 0.25rem rgb(253 13 83 / 25%';
+    }else{
         closePopUp();
-    }
+    };    
 });
 
-function closePopUp(e){
-    e.preventDefault();
+popUp_noRoom_btn.addEventListener('click', closePopUp);
+
+function closePopUp(){
     if (userName.value) {
+        if(room.value) {
+            socket.emit('join-room',  room.value, userName.value, msg => {
+                appendMsg(msg);
+            });
+        };
         userName = userName.value;
         popUp.style.display = 'none';
     }
@@ -27,7 +36,8 @@ function closePopUp(e){
 function sendMsg(e){
     e.preventDefault();
     if (input.value) {
-        socket.emit('chat message', input.value, userName);
+        socket.emit('send-message', input.value, userName);
+        appendMsg(input.value, userName);
         input.value = '';
     }
 };
@@ -37,9 +47,16 @@ form.addEventListener('keydown', function(e) {
         sendMsg(e);
     };
 });
-socket.on('chat message', function(msg, user) {
+socket.on('recieve-message', appendMsg);
+
+
+function appendMsg(msg, user) {
     var item = document.createElement('li');
-    item.innerHTML = '<strong>'+ user + ': </strong>' + msg;
+    if(user) {
+        item.innerHTML = '<strong>'+ user + ': </strong>' + msg;
+    } else {
+        item.innerHTML = '<strong class="text-primary">' + msg + '</strong>';
+    };
     messages.appendChild(item);
     messages.scrollTop = messages.scrollHeight;
-});
+} 
